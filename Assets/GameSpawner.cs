67e1e7f33587f91586a141d0824a8909816513e9
HilariousCow <aubrey.hesselgren@ludopathic.co.uk>
2015-05-16 22:14:15 +0100

    beginning to get some kind of title screen spawner thing.

--- Assets/GameSpawner.cs
+++ Assets/GameSpawner.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
-public class GameSpawner : MonoBehaviour
+public class GameSpawner : MonoBehaviour, IPointerClickHandler
 {
     //point to settings
+
+    public SliderBox SliderBoxPrefab;
+    private SliderBox _sliderBoxWhite;
+    private SliderBox _sliderBoxBlack;
 
     public MatchSettingsSO[] AllMatchStyles;
     public AIPlayer[] AllAIStyles;
@@ -16,11 +22,48 @@ public class GameSpawner : MonoBehaviour
     public MainGame MainGamePrefab;
     private MainGame _mainGameInstance;
 
+
+    public void Awake()
+    {
+        SpawnInterface();
+    }
+
+    private void SpawnInterface()
+    {
+     
+
+
+        _sliderBoxWhite = transform.InstantiateChild(SliderBoxPrefab);
+        _sliderBoxWhite.transform.localPosition = Vector3.forward * 10f;
+
+        _sliderBoxBlack = transform.InstantiateChild(SliderBoxPrefab);
+        _sliderBoxBlack.transform.localPosition = Vector3.forward * -10f;
+        _sliderBoxBlack.transform.localRotation *= Quaternion.AngleAxis(180, Vector3.up);
+
+
+        _sliderBoxWhite.Init(new List<AIPlayer>(AllAIStyles));
+        _sliderBoxBlack.Init(new List<AIPlayer>(AllAIStyles));
+
+
+        //got to make a "go" button.
+    }
+
+
     public void SpawnGame()
     {
         _mainGameInstance = (Instantiate(MainGamePrefab.gameObject) as GameObject).GetComponent<MainGame>();
 
         _mainGameInstance.Init(_chosenMatchStyle, _whitePlayer, _blackPlayer);
+
     }
-	
+
+
+    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SpawnGame();
+    }
+
+
 }
--- Assets/SliderBox.cs
+++ Assets/SliderBox.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
@@ -7,7 +8,36 @@ using UnityEngine.EventSystems;
 
 public class SliderBox : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+
+    public Transform Outside;
+
+    internal AIPlayer SelectedObject;
+    internal List<AIPlayer> AIs;
+
+
     private bool _isDragging = false;
+    private int _dragPointerID = -1;
+    private Vector3 _localStartDragPos;
+
+
+    public void Init(List<AIPlayer> items)
+    {
+
+
+        AIs = new List<AIPlayer>(items);
+        SelectedObject = GetCurrentlySelectedObject();
+    }
+
+    private AIPlayer GetCurrentlySelectedObject()
+    {
+        return (from AIPlayer n in AIs orderby Vector3.Dot(n.transform.up, transform.up) select n).FirstOrDefault();
+    }
+
+    private int GetSelectedIndex()
+    {
+        return AIs.IndexOf(GetCurrentlySelectedObject());
+    }
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,23 +48,52 @@ public class SliderBox : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
 	    if (_isDragging)
 	    {
-	        
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (_dragPointerID != -1)
+            {
+                Touch touch = Input.GetTouch(_dragPointerID);
+                ray = Camera.main.ScreenPointToRay(touch.position);
+            }
+	    }
+	    else
+	    {
+	        Quaternion targetDelta = Quaternion.Inverse(SelectedObject.transform.localRotation)*Quaternion.identity;
+	        float angle = Quaternion.Angle(targetDelta, Quaternion.identity);
+            if(angle > 0f)
+            {
+                Outside.transform.localRotation = Quaternion.Slerp(Outside.transform.localRotation, targetDelta,
+                                                                   Time.deltaTime*angle*5f);
+            }
+
 	    }
 	}
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         _isDragging = true;
+        _dragPointerID = eventData.pointerId;
+
+        _localStartDragPos =transform.InverseTransformPoint(  eventData.worldPosition );
     }
      
     public void OnDrag(PointerEventData eventData)
     {
-       
+        Vector3 currentLocalPos = transform.InverseTransformPoint( eventData.worldPosition );
+        Vector3 delta = currentLocalPos - _localStartDragPos;
+
+        Outside.localRotation *= Quaternion.AngleAxis(delta.x * 360, Vector3.forward);
+
+
+        _localStartDragPos = currentLocalPos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _isDragging = false;
+        _dragPointerID = -1;
+
+        SelectedObject = GetCurrentlySelectedObject();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
